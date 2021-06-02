9a788df5ed85800b913012369665726994efc3b0
Radek <radoslaw.piekarski@gmail.com>
2021-06-02 23:12:08 +0200

    Ex2

--- 8_kyu.py
+++ 8_kyu.py
@@ -4,6 +4,15 @@ def repeat_str(repeat, string):
     return repeat * string
 
 
+# create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
+
+def filter_list(gived_list):
+    return_list = []
+    for i in gived_list:
+        if isinstance(i, int):
+            return_list.append(i)
+    return return_list
+
 
 
 
