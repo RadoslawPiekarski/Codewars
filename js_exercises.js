26331a859387c0c02fe41ae56f20c5e3c7f9bbd6
Radek <radoslaw.piekarski@gmail.com>
2021-06-22 13:46:47 +0200

    simply js exercise

--- js_exercises.js
+++ js_exercises.js
@@ -1,8 +1,19 @@
 function silnia(givenNumber) {
     let returnNumber = 1;
-    for (let i=1; i === givenNumber ;i++) {
+    for (let i=1; i === givenNumber; i++) {
         returnNumber = returnNumber * i;
     }
+    return returnNumber;
 }
 
 silnia(4);
+
+function  dodajZakres(minNumber, maxNumber) {
+    let returnNumber = 0;
+    for (let i = minNumber; i === maxNumber; i++) {
+        returnNumber += i;
+    }
+    return returnNumber;
+}
+
+print(dodajZakres(7));
