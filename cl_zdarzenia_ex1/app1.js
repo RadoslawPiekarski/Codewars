7f40121f83cdd98bf7bec05ec5eaf3548f43e500
Radek <radoslaw.piekarski@gmail.com>
2021-07-13 02:16:38 +0200

    refactored exercise from CL

--- cl_zdarzenia_ex1/app1.js
+++ cl_zdarzenia_ex1/app1.js
@@ -2,13 +2,11 @@ let elements = document.getElementsByClassName("parent");
 
 function display_children(event){
     let children_element = this.children /* return HTML collection*/
-    console.log(children_element);
     children_element[0].style.display = "block"
 }
 
 function hide_childern(event){
     let children_element = this.children /* return HTML collection*/
-    console.log(children_element);
     children_element[0].style.display = "none"
 }
 
