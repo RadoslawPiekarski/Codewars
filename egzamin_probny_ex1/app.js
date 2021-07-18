969055ba454cefb8cfe171f4f6a1b10344acaecd
Radek <radoslaw.piekarski@gmail.com>
2021-07-18 18:07:33 +0200

    add OOP ex3 final version

--- egzamin_probny_ex1/app.js
+++ egzamin_probny_ex1/app.js
@@ -1,21 +1,15 @@
-function contArray(arr1, arr2){
-    return arr1.concat(arr2);
-}
-
-
-
 document.addEventListener("DOMContentLoaded", function () {
 
   const divElems = document.querySelectorAll("div.color");
 
-  divElems.forEach(function(el){
-    el.addEventListener("mouseover", function(){
+  divElems.forEach(function(element){
+    element.addEventListener("mouseover", function(){
       this.innerText = this.dataset.text;
       this.style.backgroundColor = "#" + Math.floor(Math.random()*16777215).toString(16);
     });
-
+    
+    element.addEventListener("mouseout", function() {
+      this.innerHTML = null;
+    });
   });
-
 });
-
-
